f6ab59a21cc93726cc46209b9db162a93d110fc8
Rachael Wright-Munn <chaelcodes@gmail.com>
2021-12-28 18:21:50 -0500

    Update remoteHack to work with v2 API
    `scp` now requires an `await`. Updated remoteHack to comply with this API!

--- scripts/remoteHack.js
+++ scripts/remoteHack.js
@@ -1,25 +1,26 @@
 import { getHackScript } from 'import.js';
 
 /* Deploys the hack script to all purchased servers
- * Differs from autoRemotHack because you can specify
+ * Differs from autoRemoteHack because you can specify
  * the target
  * args[0] - list of servers
+ * args[1] - alternate Hackscript (default from import)
  */
 export async function main(ns) {
   let myServers = ns.getPurchasedServers();
   let targetServers = ns.args[0].split(',');
   let hackScript = ns.args[1] || getHackScript();
-  myServers.map((server, index) => {
+  let scriptRam = ns.getScriptRam(hackScript);
+  for (const [index, server] of myServers.entries()) {
     ns.killall(server);
-    let scriptRam = ns.getScriptRam(hackScript);
     let serverRam = ns.getServerRam(server)[0];
     let threads = Math.floor(serverRam / scriptRam);
     let serverIndex = index % targetServers.length;
     let targetServer = targetServers[serverIndex];
-    ns.tprint(`${server} is hacking ${targetServer} with ${threads} threads.`);
-    ns.scp(hackScript, server);
+    ns.print(`${server} is hacking ${targetServer} with ${threads} threads.`);
+    await ns.scp(hackScript, server);
     if (threads > 0) {
       ns.exec(hackScript, server, threads, targetServer, threads);
     }
-  });
+  }
 }
